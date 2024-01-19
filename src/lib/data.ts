205b0a3e7471fa7165b38d8e05b2f7edf29f7f12
7incho <martin30990@gmail.com>
2024-01-18 23:10:39 -0300

    #

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,7 +13,7 @@ export const testimonials: Testimonial[] = [
     testimonial_name: "Maria Luisa",
     content_text:
       "No es porque sea mi hijo, pero no es un desarrollador cualquiera, sus habilidades van más allá de lo ordinario. Cada código que escribe tiene un toque de innovación.",
-    text_block: "Madre de @Martin",
+    text_block: "Madre de Martin",
   },
   {
     id: 2,
@@ -22,7 +22,7 @@ export const testimonials: Testimonial[] = [
     testimonial_name: "Camila Lopez",
     content_text:
       "Martin siempre fue nuestro as bajo la manga. En cada proyecto, brilla con una chispa única. Aporta una frescura creativa que simplemente no tiene precio.",
-    text_block: "Kinesiologa, hermana de @Martin",
+    text_block: "Kinesiologa, hermana de Martin",
   },
   {
     id: 3,
@@ -31,6 +31,6 @@ export const testimonials: Testimonial[] = [
     testimonial_name: "Máximo Wiersma",
     content_text:
       "Compartir con Martin va más allá del código. No solo es un genio del desarrollo, sino también una persona intuitiva y colaboradora.",
-    text_block: "Mejor amigo de @Martin",
+    text_block: "Mejor amigo de Martin",
   },
 ];
